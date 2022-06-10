a3229a3c7e7ceaef0494949209301aede3649c6a
melaniefarneti <2113289@ucc.edu.ar>
2022-06-10 15:55:58 -0300

    simplificacion de codigo

--- funciones.js
+++ funciones.js
@@ -3,196 +3,152 @@ var operandoa;
 var operandob;
 var operacion;
 
-function init(){
-    //variables estandar
-    var resultado = document.getElementById("resultado");
-    var borrar = document.getElementById("borrar")
-    var sacartodo = document.getElementById("borrartodo");
-    var suma = document.getElementById("sumar")
-    var resta = document.getElementById("restar");
-    var multiplicacion = document.getElementById("multiplicar");
-    var division = document.getElementById("dividir");
-    var igual = document.getElementById("igual");
-    var volver = document.getElementById("volver");
-    var parentesis = document.getElementById("parentesis");
-    var punto = document.getElementById("punto");
-    const uno = document.getElementById("uno");
-    const dos = document.getElementById("dos");
-    const tres = document.getElementById("tres");
-    const cuatro = document.getElementById("cuatro");
-    const cinco = document.getElementById("cinco");
-    const seis = document.getElementById("seis");
-    const siete = document.getElementById("siete");
-    const ocho = document.getElementById("ocho");
-    const nueve = document.getElementById("nueve");
-    const cero = document.getElementById("cero");
-
-    //+ variables cientifica
-    const pi = document.getElementById("pi");
-    const euler = document.getElementById("euler");
-    var factorial = document.getElementById("factorial");
-    var seno = document.getElementById("seno");
-    var coseno = document.getElementById("coseno");
-    var tangente = document.getElementById("tangente");
-    var signo = document.getElementById("signo");
-    var invseno = document.getElementById("invseno");
-    var invcoseno = document.getElementById("invcoseno");
-    var invtangente = document.getElementById("invtangente");
-    var logaritmo = document.getElementById("logaritmo");
-    var lognatural = document.getElementById("lognatural");
-    var raiz = document.getElementById("raiz");
-    var porcentaje = document.getElementById("porcentaje");
-    var exponencial = document.getElementById("exp");
-    var en = document.getElementById("en");
-    var cuadrado = document.getElementById("cuadrado");
-}
-
 //Eventos de click
-uno.onclick = function(e){
+uno.onclick = function(){
     resultado.textContent = resultado.textContent  + "1";
 }
 
-dos.onclick = function(e){
+dos.onclick = function(){
     resultado.textContent = resultado.textContent  + "2";
 }
 
-tres.onclick = function(e){
+tres.onclick = function(){
     resultado.textContent = resultado.textContent  + "3";
 }
 
-cuatro.onclick = function(e){
+cuatro.onclick = function(){
     resultado.textContent = resultado.textContent  + "4";
 }
 
-cinco.onclick = function(e){
+cinco.onclick = function(){
     resultado.textContent = resultado.textContent  + "5";
 }
 
-seis.onclick = function(e){
+seis.onclick = function(){
     resultado.textContent = resultado.textContent  + "6";
 }
 
-siete.onclick = function(e){
+siete.onclick = function(){
     resultado.textContent = resultado.textContent  + "7";
 }
 
-ocho.onclick = function(e){
+ocho.onclick = function(){
     resultado.textContent = resultado.textContent  + "8";
 }
 
-nueve.onclick = function(e){
+nueve.onclick = function(){
     resultado.textContent = resultado.textContent  + "9";
 }
 
-cero.onclick = function(e){
+cero.onclick = function(){
     resultado.textContent = resultado.textContent  + "0";
 }
 
-sacartodo.onclick = function(e){
+sacartodo.onclick = function(){
     borrartodo();
 }
 
-sumar.onclick = function(e){
+sumar.onclick = function(){
     resultado.textContent = resultado.textContent  + "+";
     operandoa = resultado.textContent;
     operacion = "+";
     limpiar();
 }
 
-restar.onclick = function(e){
+restar.onclick = function(){
     resultado.textContent = resultado.textContent  + "-";
     operandoa = resultado.textContent;
     operacion = "-";
     limpiar();
 }
 
-multiplicar.onclick = function(e){
+multiplicar.onclick = function(){
     resultado.textContent = resultado.textContent  + "×";
     operandoa = resultado.textContent;
     operacion = "×";
     limpiar();
 }
 
-dividir.onclick = function(e){
+dividir.onclick = function(){
     resultado.textContent = resultado.textContent  + "÷";
     operandoa = resultado.textContent;
     operacion = "÷";
     limpiar();
 }
 
-igual.onclick = function(e){
+igual.onclick = function(){
     operandob = resultado.textContent;
     resolver();
 }
 
 //+ eventos de click en cientifica
 //constantes
-pi.onclick = function(e){
+pi.onclick = function(){
     resultado.textContent = resultado.textContent  + "3.14";
 }
 
-euler.onclick = function(e){
+euler.onclick = function(){
     resultado.textContent = resultado.textContent  + "2.71";
 }
 
 //funciones
-factorial.onclick = function(e){
+factorial.onclick = function(){
     resultado.textContent = resultado.textContent  + "!";
     operandoa = resultado.textContent;
     operacion = "!";
     resolver();
 }
 
-seno.onclick = function(e){
+seno.onclick = function(){
     resultado.textContent = resultado.textContent  + "sen";
     operandoa = resultado.textContent;
     operacion = "sen";
     resolver();
 }
 
-coseno.onclick = function(e){
+coseno.onclick = function(){
     resultado.textContent = resultado.textContent  + "cos";
     operandoa = resultado.textContent;
     operacion = "cos";
     resolver();
 }
 
-tangente.onclick = function(e){
+tangente.onclick = function(){
     resultado.textContent = resultado.textContent  + "tg";
     operandoa = resultado.textContent;
     operacion = "tg";
     resolver();
 }
 
-signo.onclick = function(e){
+signo.onclick = function(){
     resultado.textContent = resultado.textContent  + "sign";
     operandoa = resultado.textContent;
     operacion = "sign";
     resolver();
 }
 
-logaritmo.onclick = function(e){
+logaritmo.onclick = function(){
     resultado.textContent = resultado.textContent  + "log";
     operandoa = resultado.textContent;
     operacion = "log";
     resolver();
 }
 
-lognatural.onclick = function(e){
+lognatural.onclick = function(){
     resultado.textContent = resultado.textContent  + "ln";
     operandoa = resultado.textContent;
     operacion = "ln";
     resolver();
 }
 
-raiz.onclick = function(e){
+raiz.onclick = function(){
     resultado.textContent = resultado.textContent + "√";
     operandoa = resultado.textContent;
     operacion = "√";
     resolver();
 }
 
-porcentaje.onclick = function(e){
+porcentaje.onclick = function(){
     resultado.textContent = resultado.textContent  + "%";
     operandoa = resultado.textContent;
     operacion = "%";
@@ -200,43 +156,42 @@ porcentaje.onclick = function(e){
 }
 
 //+ eventos de click en inversa
-//cambiar nombre a sen-1, cos-1, tg-1
-invseno.onclick = function(e){
+invseno.onclick = function(){
     resultado.textContent = resultado.textContent  + "invseno";
     operandoa = resultado.textContent;
     operacion = "invseno";
     resolver();
 }
 
-invcoseno.onclick = function(e){
+invcoseno.onclick = function(){
     resultado.textContent = resultado.textContent  + "invcoseno";
     operandoa = resultado.textContent;
     operacion = "invcoseno";
     resolver();
 }
 
-invtangente.onclick = function(e){
+invtangente.onclick = function(){
     resultado.textContent = resultado.textContent  + "invtangente";
     operandoa = resultado.textContent;
     operacion = "invtangente";
     resolver();
 }
 
-exp.onclick = function (e){
+exp.onclick = function(){
     resultado.textContent = resultado.textContent + "exp";
     operandoa = resultado.textContent;
     operacion = "exp";
     resolver();
 }
 
-en.onclick = function(e){
+en.onclick = function(){
     resultado.textContent = resultado.textContent + "en";
     operandoa = resultado.textContent;
     operacion = "en";
     resolver();
 }
 
-cuadrado.onclick = function(e){
+cuadrado.onclick = function(){
     resultado.textContent = resultado.textContent  + "cuadrado";
     operandoa = resultado.textContent;
     operacion = "cuadrado";
