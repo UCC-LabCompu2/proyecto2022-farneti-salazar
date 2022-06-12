613b1ba3906d7b538e4d5c0afc62bca42e80d9f0
mailensalazar16 <2116480@ucc.edu.ar>
2022-06-12 20:41:00 -0300

    agrego canvas

--- funciones.js
+++ funciones.js
@@ -285,4 +285,28 @@ function resolver(){
     borrartodo();
     resultado.textContent = res;
 }
+var bandera;
+function dibujar(event) {
+    var canvas = document.getElementById("canvasAdibujar");
+    var ctx = canvas.getContext("2d");
 
+    var posX = event.clientX;
+    var posY = event.clientY;
+    console.log(posX, posY);
+
+    canvas.onmousedown = function() {bandera=true};
+    canvas.onmouseup = function() {bandera=false};
+
+    if (bandera) {
+        ctx.fillRect(posX, posY, 5, 5);
+        ctx.fill;
+    }
+
+}
+function borrarGrafico() {
+    var canvas = document.getElementById("canvasAdibujar");
+    var ctx = canvas.getContext("2d");
+
+    canvas.width = canvas.width;
+}
+    
